3c3c1af6c28bfe228804ed66b823970a57a16b37
pizunnnn <146049905+pizunnnn@users.noreply.github.com>
2025-02-10 23:58:42 +0700

    Add files via upload

---
+++ pw_2025_233040093/tugas1/1a.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>1a.php</title>
+</head>
+<body>
+    <h2><b><i>"<?php
+        $text1 = "Topi saya Bundar";
+        $text2 = "Bundar Topi saya";
+        echo $text1 . " " . $text2;
+    ?>"</i></b></h2>
+</body>
+</html>
